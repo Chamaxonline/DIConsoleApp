3f88bef344adc014046039d3e908d26e9e321ffa
Chamath Viduranga <chamath.viduranga@airudi.com>
2022-06-24 02:15:44 +0530

    add git ignore with changes

---
+++ DIConsole/GuidGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIConsole
+{
+    public class GuidGenerator
+    {
+        public Guid Generate { get; set; } = Guid.NewGuid();
+    }
+}
---
+++ DIConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            //serviceCollection.AddScoped<GuidGenerator>();
+
+            // serviceCollection.AddSingleton<GuidGenerator>();
+
+             serviceCollection.AddTransient<GuidGenerator>();
+
+            var provider = serviceCollection.BuildServiceProvider();
+            var obj1 = provider.GetService<GuidGenerator>();
+            var obj2 = provider.GetService<GuidGenerator>();
+
+            var obj3 = provider.GetService<GuidGenerator>();
+            var obj4 = provider.GetService<GuidGenerator>();
+
+            var obj5 = provider.GetService<GuidGenerator>();
+            var obj6 = provider.GetService<GuidGenerator>();
+
+            Console.WriteLine($"Randon 1 -{obj1.Generate}");
+            Console.WriteLine($"Random2 -{obj2.Generate}");
+
+
+
+        }
+    }
+}
